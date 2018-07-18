a6249591d85eff4749d41241e4a3d6a04fb14aac
Howard Schuyler <lukeschuyler@Howards-MacBook-Pro.local>
2018-07-17 22:20:36 -0700

    migrations written for the walkthrough scrape game and walkthrough tables

---
+++ app/db/database.js
@@ -0,0 +1,10 @@
+const environment = process.env.NODE_ENV || 'development';
+const config = require('../../knexfile.js')[environment];
+const knex = require('knex')(config);
+const bookshelf = require('bookshelf')(knex);
+ 
+bookshelf.plugin(cascadeDelete);
+
+bookshelf.plugin('registry');
+
+module.exports = { knex, bookshelf }
---
+++ knexfile.js
@@ -0,0 +1,45 @@
+module.exports = {
+    test: {
+      client: 'mysql',
+      connection: {
+            host     : 'localhost',
+            user     : 'root',
+            password : '',
+            database: 'zelda_walkthroughs',
+            insecureAuth: true
+        },
+      migrations: {
+        directory: __dirname + '/app/db/migrations'
+      },
+      seeds: {
+        directory: __dirname + '/app/db/seeds'
+      }
+    },
+    development: {
+      client: 'mysql',
+      connection: {
+            host     : 'localhost',
+            user     : 'root',
+            password : '',
+            database: 'zelda_walkthroughs',
+            insecureAuth: true
+        },
+      migrations: {
+        directory: __dirname + '/app/db/migrations'
+      },
+      seeds: {
+        directory: __dirname + '/app/db/seeds'
+      }
+    },
+    production: {
+      client: 'mysql',
+      connection: process.env.DATABASE_URL,
+      migrations: {
+        directory: __dirname + '/app/db/migrations'
+      },
+      seeds: {
+        directory: __dirname + '/app/db/seeds'
+      }
+    }
+  };
+  
